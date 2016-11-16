ec4879c3e054878596eb086ca7c0b71672bcad74
Ondřej Hošek <hosek@edvlab.tuwien.ac.at>
2016-11-16 17:35:31 +0100

    initial commit

---
+++ RavuAlHemio.CentralizedLog/CentralizedLogger.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+using Serilog;
+using Serilog.Events;
+using Serilog.Extensions.Logging;
+
+namespace RavuAlHemio.CentralizedLog
+{
+    public static class CentralizedLogger
+    {
+        public static ILoggerFactory Factory { get; set; }
+
+        static CentralizedLogger()
+        {
+            Factory = new LoggerFactory();
+        }
+
+        public static void SetupConsoleLogging([CanBeNull] LogLevel? minimumLevel = null, [CanBeNull] Dictionary<string, LogLevel> logFilter = null)
+        {
+            var consoleProvider = new ConsoleLoggerProvider(
+                (text, logLevel) => LogFilter(text, logLevel, minimumLevel, logFilter),
+                true
+            );
+            Factory.AddProvider(consoleProvider);
+        }
+
+        public static void SetupFileLogging([NotNull] string applicationName, [CanBeNull] LogLevel? level = null)
+        {
+            var serilogLevelMapping = new Dictionary<LogLevel, LogEventLevel>
+            {
+                [LogLevel.Critical] = LogEventLevel.Fatal,
+                [LogLevel.Error] = LogEventLevel.Error,
+                [LogLevel.Warning] = LogEventLevel.Warning,
+                [LogLevel.Information] = LogEventLevel.Information,
+                [LogLevel.Debug] = LogEventLevel.Debug,
+                [LogLevel.Trace] = LogEventLevel.Verbose
+            };
+
+            var serilogLoggerConfig = new LoggerConfiguration();
+            if (level.HasValue)
+            {
+                serilogLoggerConfig = serilogLoggerConfig
+                    .MinimumLevel.Is(serilogLevelMapping[level.Value]);
+            }
+            serilogLoggerConfig = serilogLoggerConfig
+                .WriteTo.RollingFile(Path.Combine(AppContext.BaseDirectory, $"{applicationName}-{{Date}}.log"));
+
+            Factory.AddProvider(new SerilogLoggerProvider(serilogLoggerConfig.CreateLogger()));
+        }
+
+        static bool LogFilter(string logger, LogLevel level, [CanBeNull] LogLevel? minimumLevel = null, [CanBeNull] Dictionary<string, LogLevel> logFilter = null)
+        {
+            if (minimumLevel.HasValue && level < minimumLevel.Value)
+            {
+                return false;
+            }
+
+            if (logFilter != null)
+            {
+                LogLevel minimumLoggerLevel;
+                if (logFilter.TryGetValue(logger, out minimumLoggerLevel))
+                {
+                    if (level < minimumLoggerLevel)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
