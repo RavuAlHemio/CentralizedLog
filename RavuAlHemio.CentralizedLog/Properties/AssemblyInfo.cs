f78638c34e47105378654794777f55a54344ea0e
OndÅ?ej HoÅ¡ek <hosek@edvlab.tuwien.ac.at>
2016-11-16 17:38:21 +0100

    release v0.0.1

--- RavuAlHemio.CentralizedLog/Properties/AssemblyInfo.cs
+++ RavuAlHemio.CentralizedLog/Properties/AssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Reflection;
 [assembly: AssemblyProduct("RavuAlHemio.CentralizedLog")]
 [assembly: AssemblyCopyright("Copyright \u00A9 2016 Ond\u0159ej Ho\u0161ek")]
 
-[assembly: AssemblyVersion("0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
