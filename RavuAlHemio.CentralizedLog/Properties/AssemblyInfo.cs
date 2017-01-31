6ba38eeb5112a6d1f6cc18506c6e7eead25da3b2
Ondřej Hošek <hosek@edvlab.tuwien.ac.at>
2017-01-31 15:10:03 +0100

    remove properties provided by build system

--- RavuAlHemio.CentralizedLog/Properties/AssemblyInfo.cs
+++ RavuAlHemio.CentralizedLog/Properties/AssemblyInfo.cs
@@ -1,8 +1,3 @@
 using System.Reflection;
 
-[assembly: AssemblyTitle("RavuAlHemio.CentralizedLog")]
 [assembly: AssemblyProduct("RavuAlHemio.CentralizedLog")]
-[assembly: AssemblyCopyright("Copyright \u00A9 2016 Ond\u0159ej Ho\u0161ek")]
-
-[assembly: AssemblyVersion("0.0.4")]
-[assembly: AssemblyFileVersion("0.0.4")]
